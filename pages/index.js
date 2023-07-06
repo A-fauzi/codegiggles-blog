b5eefb4e0a96ec94f05376141e334df2f2cc51da
“UA-fauzi <akhmadfauzi0101@gmail.com>
2023-07-06 06:20:16 +0000

    uncomment newskatter in index.js

--- pages/index.js
+++ pages/index.js
@@ -91,11 +91,11 @@ export default function Home({ posts }) {
           </Link>
         </div>
       )}
-      {siteMetadata.newsletter.provider !== '' && (
-        <div className="flex items-center justify-center pt-4">
+      {/* siteMetadata.newsletter.provider !== "" && (
+        <div className="flex i tems-center justify-center pt-4">
           <NewsletterForm />
         </div>
-      )}
+      )*/}
     </>
   )
 }
